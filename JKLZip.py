e4727d125317568073807f0848db66acdb24318a
Tuomas Kontio <tuomas.kontio@gmail.com>
2019-10-18 12:01:53 +0300

    lisää dynaamiset pnro linkit etusivulle

--- JKLZip.py
+++ JKLZip.py
@@ -6,44 +6,6 @@ import os
 import urllib.request
 app = Flask(__name__)
 
-# lista vain kehityskäyttöön
-postinumerotnimet = ["40100 Jyväskylä Keskus",
-                     "40200 Mannila-Taulumäki",
-                     "40250 Ritoniemi-Lohikoski",
-                     "40270 Pappilanrinne-Pappilanvuori",
-                     "40320 Seppälän Teollisuusalue",
-                     "40340 Huhtasuo",
-                     "40400 Halssila",
-                     "40420 Jyskä",
-                     "40500 Keljo-Ristonmaa",
-                     "40520 Kuokkala-Ristikivi",
-                     "40530 Keljonkangas",
-                     "40600 Mattilanpelto",
-                     "40620 Keskussairaala-alue-Kukkumäki",
-                     "40630 Kypärämäki",
-                     "40640 Keltinmäki",
-                     "40660 Länsi-Palokka",
-                     "40700 Mäki-Matti",
-                     "40720 Nisula",
-                     "40740 Kortepohja",
-                     "40800 Vaajakoski",
-                     "40820 Vuoritsalo",
-                     "40900 Säynätsalo",
-                     "41120 Puuppola",
-                     "41140 Kuikka",
-                     "41160 Tikkakoski",
-                     "41450 Leppälahti",
-                     "41630 Oravasaari",
-                     "41710 Rutalahti-Kivisuo",
-                     "41800 Korpilahti Keskus",
-                     "41820 Saakoski",
-                     "41840 Moksi,",
-                     "41860 Rannila,",
-                     "41870 Putkilahti",
-                     "41880 Oittila",
-                     "41930 Kuohu",
-                     "41940 Vesanka"]
-
 def query_api():
     api = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/Postinumeroalueittainen_avoin_tieto/2019/paavo_9_koko_2019.px"
 
@@ -145,11 +107,14 @@ def sort_data(data):
 
 @app.route('/')
 def index():
-    return render_template('index.html', alueet=postinumerotnimet)
-
-@app.route("/40700")
-def dummyAlue():
-    return render_template("placeholderText.html", postinumero=40700, postinumeroNimi="jyväskylä")
+    data = get_data()
+    alueet = []
+    for i in range(0, len(data)):
+        alue = {}
+        alue['id'] = data[i]['id']
+        alue['nimi'] = data[i]['nimi']
+        alueet.append(alue)
+    return render_template('index.html', alueet=alueet)
 
 @app.route('/alue')
 def alue():
