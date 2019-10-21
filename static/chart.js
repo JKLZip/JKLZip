cea2592b07e630831a3f606836f9fbf55565ebbf
Tuomas Kontio <tuomas.kontio@gmail.com>
2019-10-21 12:31:22 +0300

    oikeaa dataa kaavioihin

--- static/chart.js
+++ static/chart.js
@@ -1,4 +1,5 @@
 'use strict'
+console.log(jkldata);
 // pie chart
 google.charts.load('current', {'packages':['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawChart);
@@ -8,14 +9,12 @@ function drawChart() {
 	//var data = new google.visualization.DataTable(jsonData);
 	var data = google.visualization.arrayToDataTable([
 	  ['Tiedot', 'Määrä'],
-	  ['Miehet',     8511],
-	  ['Naiset',      13442],
-	  ['Lapset',  3424],
-	  ['Opiskelijat', 5452],
+	  ['Miehet',  jkldata.He_miehet],
+	  ['Naiset',  jkldata.He_naiset],
 	]);
 
 	var options = {
-	  title: 'Dataa',
+	  title: 'Sukupuoli',
 	  backgroundColor: { fill:'transparent' },
 	  titleTextStyle: {color: 'white'}
 	};
@@ -36,14 +35,28 @@ function drawBasic() {
   data.addColumn('string', 'Ikä');
   data.addColumn('number', 'Määrä');
 
-  data.addRows([
-	["0-3v", 1115],
-	["4-6v", 2222],
-	["7-10v", 2313],
-	["11-15v", 7874],
-	["15-18v", 9995],
-  ]);
-
+      data.addRows([
+        ["0-2v", jkldata.He_0_2],
+        ["3-6v", jkldata.He_3_6],
+        ["7-12v", jkldata.He_7_12],
+        ["13-15v", jkldata.He_13_15],
+        ["16-17v", jkldata.He_16_17],
+        ["17-19v", jkldata.He_18_19],
+        ["20-24v", jkldata.He_20_24],
+        ["25-29v", jkldata.He_25_29],
+        ["30-34v", jkldata.He_30_34],
+        ["35-39v", jkldata.He_35_39],
+        ["40-44v", jkldata.He_40_44],
+        ["45-49v", jkldata.He_45_49],
+        ["50-54v", jkldata.He_50_54],
+        ["55-59v", jkldata.He_55_59],
+        ["60-64v", jkldata.He_60_64],
+        ["65-69v", jkldata.He_65_69],
+        ["70-74v", jkldata.He_70_74],
+        ["75-79v", jkldata.He_75_79],
+        ["80-84v", jkldata.He_80_84],
+        ["85-v", jkldata.He_85],
+      ]);
   var options = {
 	title: 'Ikäjakauma',
 	hAxis: {
