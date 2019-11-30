1f736207b2a62012a6be1a2a0e2534033f5c44f7
Miikka <miikka.o.ruohoniemi@student.jyu.fi>
2019-11-30 18:08:02 +0200

    muokattu alueiden tiedot jäämään auki sessionStoragen avulla

--- static/collapsible.js
+++ static/collapsible.js
@@ -1,27 +1,16 @@
 'use strict'
 window.onload = function() {
-
-    if(sessionStorage.length != 0){
+    if(sessionStorage.length > 0){
         for(var i=0;i<sessionStorage.length;i++){
             var h = getStorage("id" + i);
             document.getElementById(h).classList.add("active");
             document.getElementById(h).nextElementSibling.style.maxHeight = document.getElementById(h).nextElementSibling.scrollHeight + "px";
         }
-
-  /*  if(document.cookie.trim().length != 0){
-        for(var i=0;i<9;i++){
-            var h = haeKeksi("id" + i);
-            console.log("onload " + h);
-            document.getElementById(h).classList.add("active");
-            document.getElementById(h).nextElementSibling.style.maxHeight = document.getElementById(h).nextElementSibling.scrollHeight + "px";;
-        }
-  */} else{
+    } else{
         if(document.getElementById('first_collapsible')){
             firstCollapsible();
         }
-      }
-
-
+    }
 }
 collapse();
 
@@ -29,7 +18,7 @@ function firstCollapsible(){
         var el = document.getElementById('first_collapsible');
         el.previousElementSibling.classList.add("active");
         el.style.maxHeight = el.scrollHeight + "px";
-        //asetaKeksi();
+        setStorage();
 }
 
 function collapse(){
@@ -48,12 +37,10 @@ function collapse(){
 			    this.classList.remove("active");
 				content.style.maxHeight = null;
 				deleteStorage();
-				//asetaKeksi();
 			} else {
                 this.classList.add("active");
                 content.style.maxHeight = content.scrollHeight + "px";
                 setStorage();
-                //asetaKeksi();
             }
         });
     }
@@ -68,52 +55,17 @@ function menu_collapse(s) {
 
 function setStorage(){
     var lista = document.getElementsByClassName('collapsible active');
-
     for(var i=0;i<lista.length;i++){
-
-        sessionStorage.setItem("id" + i, lista[i]);
+        sessionStorage.setItem("id" + i, lista[i].id);
     }
 }
 
 function getStorage(cname){
-    sessionStorage.getItem(cname);
+    var x = sessionStorage.getItem(cname);
+    return x;
 }
 
-function deleteStorage(cname){
-    sessionStorage.removeItem(cname);
+function deleteStorage(){
+    sessionStorage.clear();
+    setStorage();
 }
-
-/*
-//funktio on haettu https://www.w3schools.com/js/js_cookies.asp
-function haeKeksi(id){
-    var id = id +"=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var dc = decodedCookie.split(';');
-    console.log("dc: "+dc);
-    for(var i = 0; i < dc.length; i++) {
-        var c = dc[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(id) == 0) {
-            return c.substring(id.length, c.length);
-        }
-    }
-    return "";
-}
-
-function asetaKeksi(){
-    var lista = document.getElementsByClassName("collapsible active");
-    console.log(lista);
-    if(lista.length > 0){
-        for(var i=0; i<lista.length;i++){
-            var id = lista[i].id;
-            console.log(id);
-            document.cookie = "id" + i + "="+ id + ";path=/";
-        }
-    } else{
-        document.cookie = "id0=perustiedot; path=/";
-    }
-    console.log(document.cookie);
-}
-*/
